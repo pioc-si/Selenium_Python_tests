02fb0274ac7eb90da849a1c55fb0bcced56f7f0b
pioc_si <victoriia.davydova@yandex.ru>
2023-06-08 22:31:05 +0200

    Page Object tests

--- Module4_final_project/Selenium_PyTest_final_project/test_main_page.py
+++ Module4_final_project/Selenium_PyTest_final_project/test_main_page.py
@@ -1,11 +1,17 @@
 import pytest
-from selenium import webdriver
+from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, link) #initional Page Object, example of driver and url in constructor
+    page.open()
+    page.go_to_login_page()
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
