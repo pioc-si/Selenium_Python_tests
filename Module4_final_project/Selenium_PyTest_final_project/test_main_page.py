7a52c90153b47d53892f7fff42aa3ed6547b29cd
pioc_si <victoriia.davydova@yandex.ru>
2023-06-12 22:38:55 +0200

    add LoginPage in test-case

--- Module4_final_project/Selenium_PyTest_final_project/test_main_page.py
+++ Module4_final_project/Selenium_PyTest_final_project/test_main_page.py
@@ -8,6 +8,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link) #initional Page Object, example of driver and url in constructor
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -33,4 +35,3 @@ def test_guest_should_see_register_form(browser):
     page.open()
     page.should_be_register_form()
 
-    
