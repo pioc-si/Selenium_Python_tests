0d3317c2896a209865affe20c4a58991065a77b6
pioc_si <victoriia.davydova@yandex.ru>
2023-08-23 22:26:20 +0300

    add test-cases

--- Module4_final_project/Autotests_Selenium_Python_project/test_product_page.py
+++ Module4_final_project/Autotests_Selenium_Python_project/test_product_page.py
@@ -10,8 +10,42 @@ def test_guest_can_add_product_to_basket(browser):
         page.add_product_to_basket()
         page.solve_quiz_and_get_code()
         page.should_be_success_message()
-        time.sleep(2)
+
+def test_should_be_success_message(browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_basket()
+        page.solve_quiz_and_get_code()
+        page.should_be_success_message()
+
+
+def test_should_be_correct_name_product(browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_basket()
+        page.solve_quiz_and_get_code()
+        page.should_be_success_message()
         page.should_be_correct_name_product()
+
+
+def test_should_be_basket_price_message(browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_basket()
+        page.solve_quiz_and_get_code()
+        page.should_be_success_message()
         page.should_be_basket_price_message()
-        page.should_be_correct_price_product_in_message_and_price_product()
+
+def test_should_be_correct_price_product_in_message_and_price_product(browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_basket()
+        page.solve_quiz_and_get_code()
+        page.should_be_success_message()
+        page.should_be_basket_price_message()
+        page.should_be_correct_price_product_in_message_and_price_product
 
